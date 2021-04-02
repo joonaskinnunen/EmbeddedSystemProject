11cc951659131ecbf14ccccb7dda2a204bbc3707
Esa Tervonen <t9tees00@students.oamk.fi>
2021-04-03 00:45:17 +0300

    added axios data importing

--- ReactNative/OuluWeather/Navigation/TabNavigation.js
+++ ReactNative/OuluWeather/Navigation/TabNavigation.js
@@ -4,53 +4,78 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from '@expo/vector-icons'
 import { FontAwesome5 } from '@expo/vector-icons'
 import {HomeStackScreen, TemperatureStackScreen, MoistureStackScreen, LightnessStackScreen} from '../Components/StackScreens'
+import AppContext from '../Components/AppContext'
+import axios from 'axios'
+
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
+    const [data, setData] = React.useState([])
+    const getData = () => {
+        const config = {
+            method: 'get',
+            url: 'http://localhost:3001/api/data',
+        }
+        axios(config)
+        .then(response => {
+            setData(response.data)
+        })
+        .catch(error => {
+          console.log('Error: ' + error.response.data.message)
+        })
+    }
+    React.useEffect(() => {
+        const interval_id = setInterval(getData, 1000)
+        return () => {
+            clearInterval(interval_id)
+        }
+    }, [])
     return (
-        <NavigationContainer>
-            <Tab.Navigator initialRouteName="Home"
-                screenOptions={({ route }) => ({
-                    headerStyle: {
-                        backgroundColor: '#f4511e',
-                    },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+        <AppContext.Provider value={data}>
+            <NavigationContainer>
+                <Tab.Navigator initialRouteName="Home"
+                    screenOptions={({ route }) => ({
+                        headerStyle: {
+                            backgroundColor: '#f4511e',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
 
-                        if (route.name === 'Home') {
-                            iconName = 'home'
-                        } else if (route.name === 'Temperature') {
-                            iconName = 'temperature-high'
-                        }
-                        else if (route.name === 'Moisture') {
-                            iconName = 'water'
-                        }
-                        else if (route.name === 'Lightness') {
-                            iconName = 'sunny'
-                        }
-                        return iconName == 'temperature-high' ? <FontAwesome5 name={iconName} size={size} color={color} /> : <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: 'tomato',
-                    inactiveTintColor: 'gray',
-                }}
-            >
-                <Tab.Screen name="Home" component={HomeStackScreen}
-                    options={{ title: 'Koti' }} />
-                <Tab.Screen name="Temperature" component={TemperatureStackScreen}
-                    options={{ title: 'Lämpötila' }} />
-                <Tab.Screen name="Moisture" component={MoistureStackScreen}
-                    options={{ title: 'Ilman kosteus' }} />
-                <Tab.Screen name="Lightness" component={LightnessStackScreen}
-                    options={{ title: 'Valon määrä' }} />
-            </Tab.Navigator>
-        </NavigationContainer>
+                            if (route.name === 'Home') {
+                                iconName = 'home'
+                            } else if (route.name === 'Temperature') {
+                                iconName = 'temperature-high'
+                            }
+                            else if (route.name === 'Moisture') {
+                                iconName = 'water'
+                            }
+                            else if (route.name === 'Lightness') {
+                                iconName = 'sunny'
+                            }
+                            return iconName == 'temperature-high' ? <FontAwesome5 name={iconName} size={size} color={color} /> : <Ionicons name={iconName} size={size} color={color} />;
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: 'tomato',
+                        inactiveTintColor: 'gray',
+                    }}
+                >
+                    <Tab.Screen name="Home" component={HomeStackScreen}
+                        options={{ title: 'Koti' }} />
+                    <Tab.Screen name="Temperature" component={TemperatureStackScreen}
+                        options={{ title: 'Lämpötila' }} />
+                    <Tab.Screen name="Moisture" component={MoistureStackScreen}
+                        options={{ title: 'Ilman kosteus' }} />
+                    <Tab.Screen name="Lightness" component={LightnessStackScreen}
+                        options={{ title: 'Valon määrä' }} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </AppContext.Provider>
     )
 }
 
