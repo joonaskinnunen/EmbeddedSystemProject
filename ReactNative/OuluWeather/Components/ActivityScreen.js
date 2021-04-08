e49d515144b763b7b3297cb606006dddbdd03ca2
Esa Tervonen <t9tees00@students.oamk.fi>
2021-04-08 05:28:02 +0300

    add activityscreen

---
+++ ReactNative/OuluWeather/Components/ActivityScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { View, Text, Button } from 'react-native'
+
+const ActivityScreen = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Lämpötila</Text>
+      </View>
+    )
+  }
+
+  export default ActivityScreen
--- ReactNative/OuluWeather/Navigation/TabNavigation.js
+++ ReactNative/OuluWeather/Navigation/TabNavigation.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from '@expo/vector-icons'
 import { FontAwesome5 } from '@expo/vector-icons'
-import {HomeStackScreen, TemperatureStackScreen, MoistureStackScreen, LightnessStackScreen} from '../Components/StackScreens'
+import {HomeStackScreen, TemperatureStackScreen, MoistureStackScreen, LightnessStackScreen, ActivityStackScreen} from '../Components/StackScreens'
 import AppContext from '../Components/AppContext'
 import axios from 'axios'
 
@@ -57,6 +57,9 @@ const TabNavigation = () => {
                             else if (route.name === 'Lightness') {
                                 iconName = 'sunny'
                             }
+                            else if (route.name === 'Activity') {
+                                iconName = 'walk-outline'
+                            }
                             return iconName == 'temperature-high' ? <FontAwesome5 name={iconName} size={size} color={color} /> : <Ionicons name={iconName} size={size} color={color} />;
                         },
                     })}
@@ -73,6 +76,8 @@ const TabNavigation = () => {
                         options={{ title: 'Ilman kosteus' }} />
                     <Tab.Screen name="Lightness" component={LightnessStackScreen}
                         options={{ title: 'Valon määrä' }} />
+                    <Tab.Screen name="Activity" component={ActivityStackScreen}
+                        options={{ title: 'Ulkonaliikkumisaktiivisuus' }} />
                 </Tab.Navigator>
             </NavigationContainer>
         </AppContext.Provider>
