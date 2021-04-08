42cacb5e0b23541e2bdd5b78c76f686d7e325727
esatervonen <62661104+esatervonen@users.noreply.github.com>
2021-04-08 05:29:52 +0300

    Update ActivityScreen.js

--- ReactNative/OuluWeather/Components/ActivityScreen.js
+++ ReactNative/OuluWeather/Components/ActivityScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Button } from 'react-native'
 const ActivityScreen = () => {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Lämpötila</Text>
+        <Text>Ulkonaliikkumisaktiivisuus</Text>
       </View>
     )
   }
