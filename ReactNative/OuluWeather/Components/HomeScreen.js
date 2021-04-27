4f350a9c4dda82f0f455ea79c56c6f06b4e18fc6
Esa Tervonen <t9tees00@students.oamk.fi>
2021-04-27 07:00:00 +0300

    cadd onpress navigation to homescreen

--- ReactNative/OuluWeather/Components/HomeScreen.js
+++ ReactNative/OuluWeather/Components/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { View, Text, StyleSheet, ActivityIndicator, Platform } from 'react-native'
+import { View, Text, StyleSheet, ActivityIndicator, Platform, SafeAreaView, Pressable } from 'react-native'
 import AppContext from './AppContext'
 import { Title } from 'react-native-paper'
 import { Ionicons } from '@expo/vector-icons'
@@ -18,6 +18,7 @@ const HomeScreen = ({ navigation }) => {
       })
     },
     tableCell: {
+      cursor: 'pointer',
       justifyContent: "center",
       alignItems: "center",
       padding: 5,
@@ -50,7 +51,8 @@ const HomeScreen = ({ navigation }) => {
   const dateNow = data.length > 0 ? new Date(data[data.length - 1].time) : new Date()
   dateNow.setHours(dateNow.getHours() - 3)
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', margin: "10%" }}>
+    <SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', margin: "2%" }}>
       <Title>Sää Oulussa</Title>
       <Text>{dateNow.toLocaleString()}</Text>
       <Text style={{marginBottom: 40, marginTop: 40}}>Tämä sovellus näyttää Oulun keskustan lämpötilan, ilmankosteuden ja valon määrän.{"\n"}
@@ -58,31 +60,25 @@ const HomeScreen = ({ navigation }) => {
       <View style={[styles.container, {
         flexDirection: "row",
       }]}>
-        <View style={[styles.tableCell, {
-         // borderBottomWidth: 0,
-         // borderRightWidth: 0
-        }]}>
+        <Pressable style={styles.tableCell} onPress={() => navigation.navigate('Temperature')}>
           <Text style={{fontSize: 50, fontWeight: 'bold'}}><Ionicons name='thermometer-outline' color='red' size={40} />{data.length > 0 ? data[data.length - 1].temperature > 0 ? '+' + parseFloat(data[data.length - 1].temperature).toFixed(1).toString() :  '-' + parseFloat(data[data.length - 1].temperature).toFixed(1).toString() : <ActivityIndicator size="large" color="#00ff00" />}</Text>
-        </View>
-        <View style={[styles.tableCell, {
-        //  borderBottomWidth: 0,
-        }]}>
+        </Pressable>
+        <Pressable style={styles.tableCell} onPress={() => navigation.navigate('Moisture')}>
           <Text style={{fontSize: 35}}><Ionicons name='water' color='#29aae1' size={40} />{data.length > 0 ? parseInt(data[data.length - 1].humidity) + "%" : <ActivityIndicator size="large" color="#00ff00" />}</Text>
-        </View>
+        </Pressable>
       </View>
       <View style={[styles.container, {
         flexDirection: "row"
       }]}>
-        <View style={[styles.tableCell, {
-        //  borderRightWidth: 0
-        }]}>
+        <Pressable style={styles.tableCell} onPress={() => navigation.navigate('Lightness')}>
           <Text style={{fontSize: 35}}><Ionicons name='sunny' color='yellow' size={40} />{data.length > 0 ? parseInt(data[data.length - 1].lightness) + "lx" : <ActivityIndicator size="large" color="#00ff00" />}</Text>
-        </View>
-        <View style={styles.tableCell}>
+        </Pressable>
+        <Pressable style={styles.tableCell} onPress={() => navigation.navigate('Activity')}>
           <Text style={{fontSize: 35}}><Ionicons name='walk-outline' color='#000000' size={40} />{data.length > 0 ? parseInt(data[data.length - 1].activity) : <ActivityIndicator size="large" color="#00ff00" />}</Text>
-        </View>
+        </Pressable>
       </View>
     </View>
+    </SafeAreaView>
   )
 }
 
